c2d21ff5997387dd748eac340acd414f4294fe32
Charlie <Charlie@192.168.1.18>
2023-01-22 14:54:37 +0100

    Guardamos todo el proyecto

--- src/prueba2/HolaMundo.java
+++ src/prueba2/HolaMundo.java
@@ -4,5 +4,10 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Adios Mundo");
+	    int [] array = new int [5];
+	    for(int i = 0; i< array.length; i++) {
+	    	System.out.println(" " + array[i]);
+	    }
+	    
 	}
 }
